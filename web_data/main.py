87445c570d7e65ff42ea55a1bc54678c711e283b
Sakib_Dalal <sakibdalal73@gmail.com>
2024-05-31 16:08:03 +0530

    change project

--- web_data/main.py
+++ web_data/main.py
@@ -1,10 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
 
 app = Flask(__name__)
 
+
+# Create Database
+class Base(DeclarativeBase):
+    pass
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# Create Table in database
+class Users(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    device_name: Mapped[str] = mapped_column(String(1000))
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+
+with app.app_context():
+    db.create_all()
+
+
+# Home Page
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# Login Page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Register Page
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
